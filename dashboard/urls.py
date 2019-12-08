b0f9b010ec8be881b4593f16d9bff17f33002c10
mkawsar <kawsar.diu888@gmail.com>
2019-12-08 11:22:35 +0600

    added somes page

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import dashboard
+from .views import *
 
 app_name = 'dashboard'
 
 urlpatterns = [
-    path('', dashboard, name='index')
+    path('', dashboard, name='index'),
+    path('login', login, name='login'),
+    path('checkout', checkout, name='checkout'),
+    path('cart', cart, name='cart'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -4,3 +4,15 @@ from django.shortcuts import render
 # Create your views here.
 def dashboard(request):
     return render(request, 'dashboard/index.html')
+
+
+def login(request):
+    return render(request, 'auth/login.html')
+
+
+def checkout(request):
+    return render(request, 'global/checkout.html')
+
+
+def cart(request):
+    return render(request, 'global/cart.html')
