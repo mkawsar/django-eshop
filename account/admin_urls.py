7c2b82c96f12d4755cb721c537d082cd534228b9
mkawsar <kawsar.diu888@gmail.com>
2019-12-17 09:52:45 +0600

    Created admin dashboard view

--- account/admin_urls.py
+++ account/admin_urls.py
@@ -4,5 +4,6 @@ from .admin_views import *
 app_name = 'admin-account'
 
 urlpatterns = [
-    path('login', adminLoginView, name='login')
+    path('login', adminLoginView, name='login'),
+    path('dashboard', adminDashboardView, name='dashboard')
 ]
--- account/admin_views.py
+++ account/admin_views.py
@@ -1,5 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, HttpResponseRedirect
 
 
 def adminLoginView(request):
+    if request.method == 'POST':
+        return HttpResponse(request.POST.get('username'))
     return render(request, 'admin/auth/login.html')
+
+
+def adminDashboardView(request):
+    return render(request, 'admin/dashboard/index.html')
