7b3e32594544ab6a3b209530a2dc65ddb229c278
mkawsar <kawsar.diu888@gmail.com>
2020-01-01 16:40:21 +0600

    product details store

--- product/admin_urls.py
+++ product/admin_urls.py
@@ -6,4 +6,6 @@ app_name = 'admin-product'
 urlpatterns = [
     path('list', ProductIndex.as_view(), name='index'),
     path('create', ProductAddView.as_view(), name='create'),
+    path('details/image/<int:pk>/', ProductImageUpload.as_view(), name='product-image'),
+    path('details/image/upload/<int:pk>/', ProductImage.as_view(), name='product-image-upload'),
 ]
--- product/admin_views.py
+++ product/admin_views.py
@@ -1,21 +1,20 @@
 from .models import Product
 from django.views import generic
-from django.shortcuts import render, HttpResponse
+from django.contrib import messages
+from django.shortcuts import redirect, HttpResponse
 from category.models import Category, SubCategory
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 # Create your views here.
-#def productIndex(request):
-#    return render(request, 'admin/product/index.html', {'title': 'Product'})
 class ProductIndex(LoginRequiredMixin, generic.TemplateView):
     template_name = 'admin/product/index.html'
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Product'
+        string = 'Product'
+        context['title'] = string.upper()
         return context
-
 
 
 # Product add template with POST function
@@ -24,11 +23,40 @@ class ProductAddView(LoginRequiredMixin, generic.TemplateView):
 
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['title'] = 'Add Product'
+        string = 'Add Product'
+        context['title'] = string.upper()
         context['categories'] = Category.objects.all().order_by('name')
         context['sub_categories'] = SubCategory.objects.all().order_by('sub_category_name')
         return context
 
     def post(self, request):
-        print(request.POST.get('product_details'))
-        return HttpResponse(request.POST.get('product_details'))
+        product = Product(created_by_id=request.user.id, product_name=request.POST.get('name'),
+                          product_description=request.POST.get('product_details'),
+                          category_id=request.POST.get('category_id'),
+                          sub_category_id=request.POST.get('sub_category_id'))
+        product.save()
+        messages.success(request, 'Product added to successfully!')
+        return redirect('admin-product:product-image', pk=product.id)
+
+
+class ProductImageUpload(LoginRequiredMixin, generic.DetailView):
+    model = Product
+    template_name = 'admin/product/product-image.html'
+    context_object_name = 'product'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(**kwargs)
+        string = 'Product Details'
+        context['title'] = string.upper()
+        context['categories'] = Category.objects.all().order_by('name')
+        context['sub_categories'] = SubCategory.objects.all().order_by('sub_category_name')
+        return context
+
+
+class ProductImage(LoginRequiredMixin, generic.TemplateView):
+    template_name = 'admin/product/add-image.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductImage, self).get_context_data(**kwargs)
+        context['productID'] = kwargs.get('pk')
+        return context
