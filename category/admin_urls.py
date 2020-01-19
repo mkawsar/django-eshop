45b102b4c2a78e183ec160da73837caaeecd98f8
mkawsar <kawsar.diu888@gmail.com>
2020-01-19 23:42:00 +0600

    product ss category deleted successfully

--- category/admin_urls.py
+++ category/admin_urls.py
@@ -8,8 +8,9 @@ urlpatterns = [
     path('create', AdminCreateCategoryView.as_view(), name='create-category'),
     path('sub/category/list', AdminSubCategoryListView.as_view(), name='sub-category-list'),
     path('sub/category/create', AdminSubCategoryCreateView.as_view(), name='sub-category-create'),
-    path('ss/category/list', AdminSSCategoryListView.as_view(), name='ss-category-list'),
+    path('ss/category/list', AdminSSCategoryListView, name='ss-category-list'),
     path('ss/category/create', AdminSSCategoryCreateView.as_view(), name='ss-category-create'),
     path('edit/<int:pk>', AdminCategoryEditView.as_view(), name='edit-category'),
     path('delete/<category_id>', AdminCategoryDelete, name='delete-category'),
+    path('ss/category/delete/<ss_category_id>', AdminDeleteSSCategory, name='delete-ss-category'),
 ]
